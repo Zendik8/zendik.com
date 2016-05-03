2fd304b5e67c764d42381e01c3e69b51911a73e6
Zendik <zendik8@ukr.net>
2016-05-03 23:14:24 +0100

    link checked

--- index.php
+++ index.php
@@ -3,11 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Git</title>
-<<<<<<< HEAD
     <link rel="stylesheet" href="style.css">
-=======
-    <link href="style.css" rel="stylesheet">
->>>>>>> master
 </head>
 <body>
 <h1>Learn git </h1>
